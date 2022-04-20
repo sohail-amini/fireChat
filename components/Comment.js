1fefe9ef59b1f4794256a0f8ad886832295c07fa
sohail <sohailbadghisi1@gmail.com>
2022-04-20 21:26:42 +0430

    First commit

--- components/Comment.js
+++ components/Comment.js
@@ -1,44 +1,44 @@
-import React, {useState, useEffect,useContext} from 'react';
-import { View, Text,TextInput,TouchableOpacity, StyleSheet ,FlatList,Alert} from 'react-native';
+import React, {useState, useEffect, useContext} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Alert,
+} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { fetchUsersData } from '../../redux/actions/index'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchUsersData} from '../../redux/actions/index';
 import {AuthContext} from '../navigation/AuthProvider';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Comment = ({navigation, route}) => {
-
   const {user, logout} = useContext(AuthContext);
-  const [comments, setComments] = useState([])
-  const [thePostId, setThePostId] = useState("")
-  const [text, setText] = useState("");
+  const [comments, setComments] = useState([]);
+  const [thePostId, setThePostId] = useState('');
+  const [text, setText] = useState('');
   const [updated, setUpdated] = useState(false);
   const [loading, setLoading] = useState(true);
-  
+
   const {userId, postId} = route.params;
   const [deleted, setDeleted] = useState(false);
 
   const fetchComments = async () => {
-
     const comments = [];
-    
+
     try {
-      
       firestore()
         .collection('comments')
         .doc(postId)
         .collection('data')
         .get()
-        .then((snapshot) => {
+        .then(snapshot => {
           snapshot.docs.map(doc => {
-
-            const {
-              text, 
-              userName,
-              myId
-            } = doc.data();
+            const {text, userName, myId} = doc.data();
 
             comments.push({
               commentId: doc.id,
@@ -46,28 +46,23 @@ const Comment = ({navigation, route}) => {
               userName,
               myId,
             });
-            
+
             setComments(comments);
-
-          })
-        }) 
-      
-    } catch(e) {
-
-    }
-  }
-
+          });
+        });
+    } catch (e) {}
+  };
 
   useEffect(() => {
     fetchComments();
     setUpdated(false);
     setDeleted(false);
-    navigation.addListener("focus", () => {
-      setLoading(!loading)
+    navigation.addListener('focus', () => {
+      setLoading(!loading);
     });
   }, [navigation, loading, updated, deleted]);
 
-  const handleCommentDelete = (commentId) => {
+  const handleCommentDelete = commentId => {
     Alert.alert(
       'Delete Comment',
       'Are you sure?',
@@ -86,9 +81,7 @@ const Comment = ({navigation, route}) => {
     );
   };
 
-
-  const deleteComment = async (commentId) => {
-
+  const deleteComment = async commentId => {
     firestore()
       .collection('comments')
       .doc(postId)
@@ -103,110 +96,88 @@ const Comment = ({navigation, route}) => {
         setDeleted(true);
         setComments([]);
       })
-      .catch((e) => alert('Error deleting Comment.', e));
-      
+      .catch(e => alert('Error deleting Comment.', e));
   };
 
-  
-
-  const onCommentSend = async (t) => {
-
-   
-     try {
-
+  const onCommentSend = async t => {
+    try {
       await firestore()
-      .collection('comments')
-      .doc(postId)
-      .collection('data')
-      .add({
-        userName:auth().currentUser.email,
-        text,
-        myId:auth().currentUser.uid,
-      })
+        .collection('comments')
+        .doc(postId)
+        .collection('data')
+        .add({
+          userName: auth().currentUser.email,
+          text,
+          myId: auth().currentUser.uid,
+        });
 
-      setText("")
+      setText('');
       setUpdated(true);
-
-    } catch(e) {
-      alert(e)
+    } catch (e) {
+      alert(e);
     }
-
-  }
-
-
-  
+  };
 
   return (
     <View>
       <View style={styles.commentContainer}>
-          <TextInput
-            value={text}
-            placeholder='Write your comment...'
-            onChangeText={(text) => setText(text)}
-            style={styles.inputControl} 
-            />
-            <TouchableOpacity onPress= {() => onCommentSend()} >
-              <MaterialCommunityIcons 
-                name="send"
-                color="crimson"
-                size={25}
-              />
-            </TouchableOpacity>
-        </View>
+        <TextInput
+          value={text}
+          placeholder="Write your comment..."
+          onChangeText={text => setText(text)}
+          style={styles.inputControl}
+        />
+        <TouchableOpacity onPress={() => onCommentSend()}>
+          <MaterialCommunityIcons name="send" color="crimson" size={25} />
+        </TouchableOpacity>
+      </View>
 
       <FlatList
-       keyExtractor={(item, index) => index.toString()}
-       data={comments} 
-       style={{width:'100%', marginTop:10}}
-       renderItem={({item}) => (
-         <View style={styles.commentList}>
-           <View style={{flexDirection:'row', flex:1}}>
-            <Text style={styles.creator}>
-              {item.userName} 
-              </Text>
-            <Text> {item.text} </Text>
-           </View>
-           {item.myId === user.uid ? 
-           <TouchableOpacity
-            style={{}} 
-            onPress={() => handleCommentDelete(item.commentId)}>
-              <MaterialCommunityIcons 
-                name="delete"
-                color="gray"
-                size={17}
-              />
-            </TouchableOpacity>
-            :null}
-         </View>
-      )}
+        keyExtractor={(item, index) => index.toString()}
+        data={comments}
+        style={{width: '100%', marginTop: 10}}
+        renderItem={({item}) => (
+          <View style={styles.commentList}>
+            <View style={{flexDirection: 'row', flex: 1}}>
+              <Text style={styles.creator}>{item.userName}</Text>
+              <Text> {item.text} </Text>
+            </View>
+            {item.myId === user.uid ? (
+              <TouchableOpacity
+                style={{}}
+                onPress={() => handleCommentDelete(item.commentId)}>
+                <MaterialCommunityIcons name="delete" color="gray" size={17} />
+              </TouchableOpacity>
+            ) : null}
+          </View>
+        )}
       />
     </View>
   );
-}
+};
 
 export default Comment;
 
 const styles = StyleSheet.create({
-  
   commentContainer: {
-    width:'100%',
-    flexDirection:'row',
-    alignItems:'center',
-    paddingHorizontal:10,
-    backgroundColor:'white',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: 'white',
   },
   inputControl: {
-    flex:1
+    flex: 1,
   },
   commentList: {
     padding: 10,
-    backgroundColor:'white',
-    borderBottomWidth:.5,
-    borderColor:'#aaa',
-    flexDirection:'row'
+    backgroundColor: 'white',
+    borderBottomWidth: 0.5,
+    borderColor: '#aaa',
+    flexDirection: 'row',
   },
   creator: {
-    fontWeight:'bold',
-    marginBottom:5
-  }
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
 });
